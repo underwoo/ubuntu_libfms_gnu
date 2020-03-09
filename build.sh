8b5238e014d5d1cdc37c4c0ff45d6a8171e5ab25
Seth Underwood <underwoo@underwoo.io>
2020-03-09 19:51:30 -0400

    Attempt for GitHub action container

--- build.sh
+++ build.sh
@@ -6,26 +6,12 @@
 # Ensure if any command failes, then the script fails.
 set -e
 
-# The clone is _hopefully_ only needed during testing
-git clone https://github.com/NOAA-GFDL/FMS.git
-
-# Using an environment variable to get a particular hash/tag/branch (if it
-# is set)
-if test ! -z $BUILD_VERSION
-then
-   ( cd FMS && git checkout $BUILD_VERSION )
-fi
-
-# Prepare the source directory for building
-( cd FMS && autoreconf -if )
-
-# Create the new build directory, and run configure
-mkdir build
-cd build
+# Prepare for the build
+autoreconf -if
 
 # All environment variables (e.g. CC, FC, FCFLAGS, etc) should be set
 # via enviornment variables.
-../FMS/configure
+./configure
 
 # Build the library
 make -j distcheck
--- startup.sh
+++ startup.sh
@@ -4,13 +4,6 @@
 # Force script to fail on any command failure
 set -e
 
-# If the first argument to this script is build.sh, then the script will build
-# the library under the "builder" user.
-if test "$1" = "/build.sh"
-then
-   exec gosu builder "$@"
-fi
-
 # The following is needed for the Docker CMD.  In the default case, this
 # should run the builder script (build.sh).  However, someone can override this
 # with a `docker run <image> <command>`.
